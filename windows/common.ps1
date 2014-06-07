8e2407047af20adaec7a83c07e0fa965808c5fbc
Vince Cima <contact@vincecima.com>
2014-06-07 13:42:20 -0500

    Boxstarter scripts for Windows boxes

---
+++ windows/common.ps1
@@ -0,0 +1,14 @@
+Update-ExecutionPolicy
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showFileExtensions
+ 
+cinst 1password
+cinst 7zip
+cinst dropbox
+cinst GoogleChrome
+cinst lockhunter
+cinst powershell4
+cinst sublimetext2
+cinst sumatrapdf.install
+cinst TeraCopy
+cinst winmerge
+cinst winsplit
